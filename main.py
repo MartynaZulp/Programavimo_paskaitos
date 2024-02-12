9bff1ad2695826f805b4ed23f81b3b3e06422c6d
MartynaZulp <159789417+MartynaZulp@users.noreply.github.com>
2024-02-12 17:38:53 +0200

    Update main.py
    Pridetos paprastos skaiciuotuvo funkcijos

--- main.py
+++ main.py
@@ -9,4 +9,13 @@ def funkcija():
     print(f'test:{kintamasis2}{kintamasis}')
     print('test:{}{}'.format({kintamasis2,kintamasis}))
 
+def sudetis(pirmas, antras):
+    return pirmas+antras
+def atimtis(pirmas, antras):
+    return pirmas-antras
+def daugyba (pirmas, antras):
+    return pirmas*antras
+def dalyba(pirmas, antras):
+    return pirmas/antras
+    
 
