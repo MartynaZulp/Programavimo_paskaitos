f5bda7b2a16ee88761b2ef34950fa57d8912f9af
MartynaZulp <159789417+MartynaZulp@users.noreply.github.com>
2024-02-12 17:46:19 +0200

    Update main.py
    Prideti komentarai paprastoms skaiciuotuvo funkcijoms ir pridetas pradzios ekrano tekstas rodomas vartotojui norinciam pasirinkti funkcijas

--- main.py
+++ main.py
@@ -9,13 +9,25 @@ def funkcija():
     print(f'test:{kintamasis2}{kintamasis}')
     print('test:{}{}'.format({kintamasis2,kintamasis}))
 
+# Funkcija sudeda du skaicius
 def sudetis(pirmas, antras):
     return pirmas+antras
+# Funkcija is pirmo atima antraji skaiciu
 def atimtis(pirmas, antras):
     return pirmas-antras
+# Funkcija daugina viena skaiciu is kito
 def daugyba (pirmas, antras):
     return pirmas*antras
+# Funkcija dalina viena skaiciu is kito
 def dalyba(pirmas, antras):
     return pirmas/antras
-    
+
+print("Pasirinkite skaiciuotuvo operacija -\n" \
+        "1. Sudetis\n" \
+        "2. Atimtis\n" \
+        "3. Daugyba\n" \
+        "4. Dalyba\n")
+
+
+
 
